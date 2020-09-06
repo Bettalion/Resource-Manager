93c329c96ac0668265597fdefc4a9455fc8c2e1c
Bettalion <Bettalion04@hotmail.com>
2020-09-06 19:07:12 +0100

    Testing, expoloring ideas

---
+++ ResourceFinder.py
@@ -0,0 +1,12 @@
+import os
+import random
+
+f=open('/Users/amoghsimhadri/BettalionFolder/cx/My Resources','r')
+data=f.read().split('\n')
+
+RevisionMedia = []
+temp = []
+i = 0 # index
+while x[i] != ' ':
+ temp.append(x[i]);
+ 
