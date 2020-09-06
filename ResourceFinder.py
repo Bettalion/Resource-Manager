8196151ca89748b6b605c87c302ce8c855ad65a5
Bettalion <Bettalion04@hotmail.com>
2020-09-06 19:30:47 +0100

    Source types

--- ResourceFinder.py
+++ ResourceFinder.py
@@ -1,12 +1,27 @@
 import os
 import random
 
-f=open('/Users/amoghsimhadri/BettalionFolder/cx/My Resources','r')
+f=open('/Users/amoghsimhadri/BettalionFolder/cx/Resource-Manager/My Resources','r')
 data=f.read().split('\n')
+if len(data) == 0:
+  print('Invalid Data!')
+  quit()
+
 
 RevisionMedia = []
 temp = []
+
 i = 0 # index
-while x[i] != ' ':
- temp.append(x[i]);
- 
+while i <len(data):
+  if data[i] != '':
+   temp.append(data[i])
+  else:
+    RevisionMedia.append(temp)
+    temp=[]
+  i+=1
+RevisionMedia.append(temp)
+
+
+Sources = [s[0] for s in RevisionMedia] 
+
+
